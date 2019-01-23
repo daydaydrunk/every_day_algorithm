7d37cbb91f529bc62144d139b890698b401f3b9f
chaihanlin <chaihanlin@luojilab.com>
2019-01-23 18:27:32 +0800

    fmt files

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -18,7 +18,42 @@ pub mod code{
 
         //leetcode 151
         pub fn re_words(s:&str) -> String{
-            "".to_string()
+            s.split_whitespace().rev().fold(String::new(),|acc,x| acc + x + " ").trim().to_string()
+        }
+
+        pub fn max_product(nums: Vec<i32>) -> i32 {
+            let mut res:i32 = 0;
+            match nums.len() {
+                1 => nums[0],
+                _ => {
+                    for (i, n) in nums.iter().enumerate() {
+                        let p = match nums.get(i + 1) {
+                            Some(j) => {
+                                if (j - *n).abs() == 1 { j * (*n) } else { 0 }
+                            },
+                            None => *n,
+                        };
+                        if p > res { res = p; }
+                    }
+                    res
+                },
+            }
+        }
+
+        pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+            match nums.len() {
+                0 => 0,
+                1 => nums[0],
+                _ => {
+                    let mut p = nums[0];
+                    let mut s=0;
+                    for n in nums.iter() {
+                        let max = s.max(s+n);
+                        p=p.max(s);
+                    }
+                    p
+                }
+            }
         }
     }
 }
@@ -35,17 +70,24 @@ fn test_rob(){
 #[test]
 fn test_re_words(){
     let mut str1 = "this is a   pen  ";
-    println!("{:?}",code::Solution::re_words(str1));
+    assert_eq!(code::Solution::re_words(str1),"pen a is this");
+}
 
+#[test]
+fn test_max_product(){
+    assert_eq!(code::Solution::max_product(vec![2,3,0,4]),6);
+    assert_eq!(code::Solution::max_product(vec![-2,0,-1]),0);
+    assert_eq!(code::Solution::max_product(vec![-2,0,-1,-3,-4]),12);
+    assert_eq!(code::Solution::max_product(vec![-2]),-2);
+    assert_eq!(code::Solution::max_product(vec![2]),2);
+    assert_eq!(code::Solution::max_product(vec![0]),0);
+    assert_eq!(code::Solution::max_product(vec![-1,-2,-3,9,-8,-9]),72);
+}
 
-
-//    for c in str1.split_whitespace().collect::<Vec<&str>>() {
-//        s1.push(c);
-//        println!("{:?}",c);
-//    }
-    let mut s1 = vec!["a","b","c"];
-    println!("{:?} {:?}",s1.into_iter().map(| x| { x.to_owned() + "c"}).collect::<Vec<String>>(),1);
-    assert_eq!(str1,"pen a is this");
+#[test]
+fn test_max_sub_array(){
+    assert_eq!(code::Solution::max_sub_array(vec![-2,1,-3,4,-1,2,1,-5,4]),6);
+    assert_eq!(code::Solution::max_sub_array(vec![-1]),-1);
 }
 
 //EOF
